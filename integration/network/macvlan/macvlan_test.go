tiborvass/docker

Integration: MACVlan add missing import comment and build-tag
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

integration/network/macvlan/macvlan_test.go
@@ -1,6 +1,6 @@
 // +build !windows
 
-package macvlan
+package macvlan // import "github.com/tiborvass/docker/integration/network/macvlan"
 
 import (
 	"context"

integration/network/macvlan/main_windows_test.go
@@ -1,0 +1,1 @@
+package macvlan // import "github.com/tiborvass/docker/integration/network/macvlan"

wang371758198/engine

(cherry picked from commit 316e16618f8c794b8e56e223ed0bed15e1f4be24)

integration/network/macvlan/macvlan_test.go
@@ -1,6 +1,6 @@
 // +build !windows
 
-package macvlan
+package macvlan // import "github.com/docker/docker/integration/network/macvlan"
 
 import (
 	"context"

integration/network/macvlan/main_windows_test.go
@@ -1,0 +1,1 @@
+package macvlan // import "github.com/docker/docker/integration/network/macvlan"